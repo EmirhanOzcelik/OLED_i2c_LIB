1fed28225d2245bac5ea65eb05f82f6b67169273
emirhan_ozclk <94894644+EmirhanOzcelik@users.noreply.github.com>
2025-05-30 14:26:34 +0300

    Add files via upload

---
+++ OLED_128x64_LIB/fontlar.h
@@ -0,0 +1,25 @@
+#pragma once
+#include "Arduino.h"
+#include "pgmspace.h"
+
+extern uint8_t genelFont[];
+extern uint8_t buyuk_numara[];
+extern uint8_t orta_numara[];
+
+// --- Büyük Harfler ---
+
+extern uint8_t turkce_buyuk_C[]; // Ç
+extern uint8_t turkce_buyuk_G[]; // Ğ
+extern uint8_t turkce_buyuk_I[]; // İ
+extern uint8_t turkce_buyuk_O[]; // Ö
+extern uint8_t turkce_buyuk_S[]; // Ş
+extern uint8_t turkce_buyuk_U[]; // Ü
+
+// --- Küçük Harfler ---
+
+extern uint8_t turkce_kucuk_c[];          // ç
+extern uint8_t turkce_kucuk_g[];          // ğ
+extern uint8_t turkce_kucuk_i_noktasiz[]; // ı
+extern uint8_t turkce_kucuk_o[];          // ö
+extern uint8_t turkce_kucuk_s[];          // ş
+extern uint8_t turkce_kucuk_u[];          // ü
